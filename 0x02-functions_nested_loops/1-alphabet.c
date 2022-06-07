fe35607bfb3373aae8b187eb28524924b1832f84
Torgbor-Richter <torgborrichter23@gmail.com>
2022-06-07 15:50:30 -0700

    game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_alphabet -> prints the lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	int j;
+	for (j = 'a' ; j <= 'j' , j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\');
