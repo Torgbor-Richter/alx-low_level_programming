3aded47e5d46fcf08f54b149b25498687dbc2a86
Torgbor-Richter <torgborrichter23@gmail.com>
2022-06-10 06:01:02 -0700

    print

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,8 +7,8 @@
 void print_diagonal(int n)
 {
 	int x, i;
-
-	if (n <= 0)
+	
+	if (n <= o)
 		_putchar('\n');
 	else
 	{
@@ -17,7 +17,8 @@ void print_diagonal(int n)
 			for (i = 0; i < x; i++)
 				_putchar(' ');
 			_putchar(92);
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
+
