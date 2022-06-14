e74063167432bf0abe102c49958271dab2c74c44
Torgbor-Richter <torgborrichter23@gmail.com>
2022-06-13 19:01:13 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,8 @@ void puts2(char *str)
 {
 	int len, i;
 
-	len = strlen(str)
-		for (i = 0; i < len; i += 2)
-			_putchar(str[i]);
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }
