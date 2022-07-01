4a669c330273673e68fe7b3684a45aea937b6740
Torgbor-Richter <torgborrichter23@gmail.com>
2022-07-01 04:31:41 -0700

    static libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * _atoi - converts a string to an integer
@@ -28,8 +28,8 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-			if (d % 2
-					digit = -digit;)
+			if (d % 2)
+					digit = -digit;
 				n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
